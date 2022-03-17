5df22f8ae8149b814e851611608c9172cbb0e604
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-03-17 07:34:02 +0100

    Create 02_median.py
    Purpose: Calculate the median of a given list of numbers (integer, float).

---
+++ 02_median.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 02_median.py
+
+# Purpose: Calculate the median of a given list of numbers (integer, float).
+
+def calculate_median(numbers):
+    N = len(numbers)
+    numbers.sort()
+
+    if N % 2 == 0:
+        m1 = N/2
+        m2 = (N/2) + 1
+        m1 = int(m1) - 1
+        m2 = int(m2) - 1
+        median = (numbers[m1] + numbers[m2])/2
+    else:
+        m = (N + 1) / 2
+        m = int(m) - 1
+        median = numbers[m]
+    
+    return median
+
+if __name__ == '__main__':
+    numbers = [10, 6, 7, 90, 10, 20, 50, 5, 53, 60, 10, 10]
+    median = calculate_median(numbers)
+    print('Median number is: {0}'.format(median))

5b5710ab859334ef6e51488e650b47cc18386736
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-03-17 07:01:51 +0100

    Create 01_mean.py
    Purpose: Calculate the mean of a given list of numbers (integer, float).

---
+++ 01_mean.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 01_mean.py
+
+# Purpose: Calculate the mean of a given list of numbers (integer, float).
+
+def calculate_mean(numbers):
+    s = sum(numbers)
+    N = len(numbers)
+    mean = s/N
+    return mean
+
+if __name__ == '__main__':
+    numbers = [10, 6, 7, 90, 10, 20, 50, 5, 53, 60, 10, 12]
+    mean = calculate_mean(numbers)
+    N = len(numbers)
+    print('Mean number is: {0}'.format(mean))

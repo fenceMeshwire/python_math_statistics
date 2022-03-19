4e756bbc0832f6bf7149c779d32e4f761bc6f1e9
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-03-19 13:52:58 +0100

    Update 01_mean.py
    Purpose: Calculate the mean of a given list of numbers (integer, float).
    The mean (average) of a data set is found by adding all numbers in the data set 
    and then dividing by the number of values in the set

--- 01_mean.py
+++ 01_mean.py
@@ -5,6 +5,8 @@
 # 01_mean.py
 
 # Purpose: Calculate the mean of a given list of numbers (integer, float).
+# The mean (average) of a data set is found by adding all numbers in the data set 
+# and then dividing by the number of values in the set.
 
 def calculate_mean(numbers):
     s = sum(numbers)
@@ -16,3 +18,4 @@ if __name__ == '__main__':
     numbers = [10, 6, 7, 90, 10, 20, 50, 5, 53, 60, 10, 12]
     mean = calculate_mean(numbers)
     print('Mean number is: {0}'.format(mean))
+    
